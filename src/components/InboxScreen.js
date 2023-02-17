65ca0e20073cae52b6479cddd21e38fad4d049eb
Babi1618 <b.giuliani1618@gmail.com>
2023-02-17 11:04:58 +0100

    GitHub action setup

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -18,7 +18,7 @@ export default function InboxScreen() {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
-          <p className="title-message">Oh no!!!</p>
+          <p className="title-message">Oh no!!! </p>
           <p className="subtitle-message">Something went wrong...</p>
         </div>
       </div>
@@ -27,7 +27,7 @@ export default function InboxScreen() {
   return (
     <div className="page lists-show">
       <nav>
-        <h1 className="title-page">Taskbox</h1>
+        <h1 className="title-page">Taskbox!</h1>
       </nav>
       <TaskList />
     </div>
